eb143e9f618e1c5f0f4e60adb0383d70b04a1a57
siddharth-koolioai <siddharth@koolio.ai>
2020-08-31 11:28:23 +0530

    updated config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_6zU9abKdA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ifkciuk5mk3d1hn6c3q9v0qh1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
